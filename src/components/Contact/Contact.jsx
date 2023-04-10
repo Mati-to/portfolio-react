083a29eff7fea56c4704757e2578ef6c0410b0e0
Matías Alfaro <matiasalfaro2.a@gmail.com>
2023-04-10 17:30:56 -0400

    Contact form done.

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -10,11 +10,12 @@ function Contact() {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
-      .then((result) => {
-        console.log(result.text);
-      }, (error) => {
-        console.log(error.text);
+    emailjs.sendForm('service_xo3n1yp', 'template_41awg8u', form.current, 'griSlYuo5qRKEMarX')
+      .then(() => {
+        alert('Message sent successfully!');
+        window.location.reload(false);
+      }, () => {
+        alert('Failed to send message. Pleasy try again.');
       });
   };
 
@@ -27,6 +28,8 @@ function Contact() {
           <input type="text" name="user_name" placeholder='Name' required />
           <label>Email</label>
           <input type="email" name="user_email" placeholder='Email' required />
+          <label>Subject</label>
+          <input type="text" name="subject" placeholder='Subject' required />
           <label>Message</label>
           <textarea name="message" rows='5' placeholder='Write here...' required />
           <input className='button' type="submit" value="Send" />
